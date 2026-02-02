b69f192c6054432a2a741d96ba853a130824969b
Nathan Nguyen <142696114+NathanNewwin@users.noreply.github.com>
2026-02-01 19:40:38 -0800

    update hello-html-python

--- nathanguyen.site/public_html/hw2/python/hello-html-python.py
+++ nathanguyen.site/public_html/hw2/python/hello-html-python.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 import datetime
-import socket
 
 t = datetime.datetime.now()
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
 
 print("Content-Type: text/html")
 print()
@@ -20,8 +17,6 @@ print(f"""<!DOCTYPE html>
   <h1 align=center>Hello, World!</h1>
   <p>This page was generated using Python CGI.</p>
   <p>This program was generated at: {t}</p>
-  <p>Your computer name is: {hostname}</p>
-  <p>Your current IP Address is: {IPAddr}</p>
 </body>
 </html>
 """)
